1650b5dbf3812bc08dbb0ed199767d674d185892
sumit075 <Lovely@123>
2015-10-10 18:54:13 -0500

    added fb button

--- app/src/main/java/com/example/sumit/wannago/MainActivity.java
+++ app/src/main/java/com/example/sumit/wannago/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
-
+//
         return super.onOptionsItemSelected(item);
     }
 }
